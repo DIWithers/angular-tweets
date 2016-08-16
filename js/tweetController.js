5ad1177412655d88ba8db8d15ce465dbf829ab5c
DIWithers <d.ivy.withers@gmail.com>
2016-08-16 16:47:35 -0400

    added 3 twitter feeds

--- js/tweetController.js
+++ js/tweetController.js
@@ -1,18 +1,71 @@
-tweetApp.controller("tweetController", function($scope, $http) {
-	$scope.test = "I am working";
-	$scope.tweets =  tweets;
+tweetApp.controller("tweetController", function($scope, $http, $location, $routeParams) {
+// console.log($scope);
 
+	console.log($routeParams);
+	if ($routeParams.searchTerm) {
+		var searchTerm = $routeParams.searchTerm;
+	}
+	else {
+		var searchTerm = 'trump';
+	}
 
 	$http({
 		method: "GET",
-		url: "http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=trump"
+		url: "http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=" + searchTerm
 	}).then(function successFunction(tweetData) {
 		$scope.tweets = tweetData.data.statuses;
-		console.log(tweetData.data.statuses);
+		// console.log(tweetData.data.statuses);
 
 	}, function failureFunction(tweetData) {
-		console.log(tweetData);
+		// console.log(tweetData);
+
+	});
+	$http({
+		method: "GET",
+		url: "http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=hillaryclinton"
+	}).then(function successFunction(hillaryTweetData) {
+		$scope.hillaryTweets = hillaryTweetData.data.statuses;
+		console.log(hillaryTweetData.data.statuses);
+
+	}, function failureFunction(tweetData) {
+		// console.log(hillaryTweetData);
+
+	});
+	$http({
+		method: "GET",
+		url: "http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=berniesanders"
+	}).then(function successFunction(bernieTweetData) {
+		$scope.bernieTweets = bernieTweetData.data.statuses;
+		console.log(bernieTweetData.data.statuses);
+
+	}, function failureFunction(tweetData) {
+		// console.log(hillaryTweetData);
 
 	});
 	
+	
+
+	
 });
+tweetApp.factory('myFactory', function(){
+    var factory = {};
+
+    factory.getVar = function(){
+        return this.variable;
+    }
+
+    factory.setVar = function(data){
+        this.variable = data;
+    }
+    
+    return factory;
+});
+
+tweetApp.controller('MainController', function($scope, myFactory){
+    myFactory.setVar("this is a variable");
+});
+
+tweetApp.controller('AnotherController', function($scope, myFactory){
+    console.log(myFactory.getVar());
+    // "this is a variable"
+})
--- js/tweetModule.js
+++ js/tweetModule.js
@@ -1,27 +1,27 @@
 var tweetApp = angular.module("tweetApp", ["ngRoute"]);
 
-tweetApp.directive("customMenu", function() { //camelCase when defined, snake-case when called
-	return {
-		restrict: "E", //E = Element, A = Attribute, C = Class, M = Comment
-		// template: "Hello, Class! I'm an arrtibute! {{test}}",
-		templateUrl: "views/menu.html"
-	}
-});
+// tweetApp.directive("customMenu", function() { //camelCase when defined, snake-case when called
+// 	return {
+// 		restrict: "E", //E = Element, A = Attribute, C = Class, M = Comment
+// 		// template: "Hello, Class! I'm an arrtibute! {{test}}",
+// 		templateUrl: "views/menu.html"
+// 	}
+// });
 
-// tweetApp.config(function($routeProvider) {
-// 	// at '/', load home.html with home controller
-// 	$routeProvider.when("/", {
-// 		templateUrl: "views/home.html",
-// 		controller: "home.controller"
-// 	});
-// 	$routeProvider.when("/trump", {
-// 		templateUrl: "views/home.html",
-// 		controller: "trumpController"
-// 	});
+tweetApp.config(function($routeProvider) {
+	// at '/', load home.html with home controller
+	$routeProvider.when("/", {
+		templateUrl: "views/home.html",
+		controller: "tweetController"
+	});
+	$routeProvider.when("/:searchTerm*", {
+		templateUrl: "views/home.html",
+		controller: "trumpController"
+	});
 // 	$routeProvider.when("/hillary", {
 // 		templateUrl: "views/home.html",
 // 		controller: "hillaryController"
 // 	});
 // 	$routeProvider.otherwise("/"); 
 
-// });
+});
